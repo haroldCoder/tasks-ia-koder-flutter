a7a85515bedab1f4aaac32167ae7cdedfa7a28cc
haroldCoder <haroldc2005@gmail.com>
2025-02-09 00:05:01 +0100

    feature: add title and icon for help in appbar and in body app add a search widget created as component

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tasks_ia_koderx/src/widgets/Search.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -20,7 +21,33 @@ class _MyHomePageState extends State<MyHomePage> {
                 Container(
                     width: double.infinity,
                     height: 65,
-                    decoration: BoxDecoration(color: Colors.blueAccent))
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    decoration: BoxDecoration(color: Colors.blueAccent),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Tasks Koderx", style: TextStyle(color: Colors.white,
+                          fontFamily: "recursive", fontSize: 21,
+                          decoration: TextDecoration.none)),
+                      Icon(
+                        Icons.help,
+                        size: 30,
+                        color: Color(0xFFFFFFFF),
+                      )
+                    ],
+                  )),
+                SizedBox(
+                  width: 250,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: const SearchWidget(margin: 20),
+                      )
+                    ],
+                  ),
+                )
               ],
             )));
   }
---
+++ lib/src/widgets/Search.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class SearchWidget extends StatefulWidget {
+  const SearchWidget({super.key, this.margin = 0});
+  final double margin;
+  @override
+  State<StatefulWidget> createState() => _SearchState();
+}
+
+class _SearchState extends State<SearchWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.only(top: widget.margin),
+        foregroundDecoration: BoxDecoration(
+            color: Colors.white12,
+            borderRadius: BorderRadius.all(Radius.circular(15))),
+        padding: EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
+        child: Row(mainAxisSize: MainAxisSize.min, children: [
+          Icon(
+            Icons.search,
+            size: 25,
+            color: Colors.white54,
+          ),
+          Expanded(
+            child: SizedBox(
+              height: 28,
+              child: Theme(data: ThemeData(
+                textSelectionTheme: TextSelectionThemeData(
+                  cursorColor: Colors.blueAccent
+                )
+              ), child: SearchBar(
+                textInputAction: TextInputAction.done,
+                hintText: "Buscar tareas...",
+                backgroundColor: MaterialStateProperty.all(Colors.transparent),
+                hintStyle: MaterialStateProperty.all(TextStyle(
+                    color: Colors.white54,
+                    fontSize: 15,
+                    decoration: TextDecoration.none,
+                    fontFamily: 'inter',
+                    decorationColor: Colors.white54
+                )
+                ),
+                textStyle: MaterialStateProperty.all(TextStyle(
+                    color: Colors.white
+                )),
+              ),)
+            ),
+          )
+        ]));
+  }
+}
