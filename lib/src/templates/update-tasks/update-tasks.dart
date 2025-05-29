f58a4a863dadd65e07a39141d86b05354debb4f9
haroldCoder <haroldc2005@gmail.com>
2025-05-28 22:22:35 -0500

    feature: refactor popUp edit task
    add style of difuminated to container popUp and changes colors

--- lib/src/templates/update-tasks/update-tasks.dart
+++ lib/src/templates/update-tasks/update-tasks.dart
@@ -88,143 +88,175 @@ class _UpdateTasksState extends State<UpdateTasks> {
 
   @override
   Widget build(BuildContext context) {
-    return PopUp(
-        border: Border(
-            right: BorderSide(color: Colors.blueAccent, width: 2),
-            left: BorderSide(color: Colors.blueAccent, width: 2),
-            top: BorderSide(color: Colors.blueAccent, width: 2),
-            bottom: BorderSide(color: Colors.blueAccent, width: 2)),
-        actions: [
-          Container(
-            alignment: Alignment.bottomRight,
-            child: Button(
-              click: () {
-                widget.updateDate(context);
-              },
-              contentbtn: Text("Actualizar",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17,
-                      fontFamily: 'inter',
-                      fontWeight: FontWeight.bold)),
-              style: ButtonStyle(
-                  shape: WidgetStatePropertyAll<OutlinedBorder>(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16))),
-                  backgroundColor:
-                      WidgetStatePropertyAll<Color>(Colors.blueAccent)),
-            ),
-          )
-        ],
-        child: Material(
-          color: Colors.transparent,
-          child: Container(
-              margin: EdgeInsets.only(top: 15),
-              decoration: BoxDecoration(color: Colors.transparent),
-              child: SingleChildScrollView(
-                child: Column(
-                  spacing: 10,
-                  children: [
-                    TextInputKoder(
-                        decoration: InputDecoration(
-                          fillColor: Colors.white,
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: const BorderSide(
-                                color: Colors.blueAccent, width: 2),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: const BorderSide(
-                                color: Colors.blueAccent, width: 2),
-                          ),
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(1.0),
-                              borderSide: BorderSide(
-                                  color: Colors.blueAccent, width: 1)),
-                        ),
-                        onChange: widget.changeTitle,
-                        value: TextEditingController(text: widget.title)),
-                    TextBoxs(
-                        height_container: 220,
-                        max_lines: 10,
-                        min_lines: 8,
-                        decoration: InputDecoration(
-                          fillColor: Colors.white,
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: const BorderSide(
-                                color: Colors.blueAccent, width: 2),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: const BorderSide(
-                                color: Colors.blueAccent, width: 2),
-                          ),
-                          hintStyle: TextStyle(
-                            color: Colors.grey[400],
-                          ),
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8.0),
-                              borderSide: BorderSide(
-                                  color: Color(0xBCC1CAFF), width: 2)),
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 15, horizontal: 15),
-                        ),
-                        onChange: widget.changeDescription,
-                        value: TextEditingController(text: widget.description)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          alignment: Alignment.bottomLeft,
-                          child: Select(
-                              onchange: widget.changePriority,
-                              decoration: ShadDecoration(
-                                  color: Colors.white,
-                                  border: ShadBorder(
-                                      radius: BorderRadius.circular(8.0),
-                                      right: ShadBorderSide(
-                                          color: Colors.blueAccent, width: 2),
-                                      left: ShadBorderSide(
-                                          color: Colors.blueAccent, width: 2),
-                                      top: ShadBorderSide(
-                                          color: Colors.blueAccent, width: 2),
-                                      bottom: ShadBorderSide(
-                                          color: Colors.blueAccent, width: 2))),
-                              options: {"1": 'Alta', "2": 'Media', "3": 'Baja'},
-                              placeholder: widget.priority == 1
-                                  ? Text("Alta")
-                                  : widget.priority == 2
-                                      ? Text("Media")
-                                      : Text("Baja")),
-                        ),
-                        Button(
-                          click: () {
-                            changeStateTask();
-                          },
-                          style: ButtonStyle(
-                            backgroundColor: WidgetStatePropertyAll<Color>(
-                                Colors.transparent),
-                            padding: WidgetStatePropertyAll<EdgeInsets>(
-                                EdgeInsets.zero),
-                            minimumSize:
-                                WidgetStatePropertyAll<Size>(Size(0, 0)),
-                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                          ),
-                          contentbtn: Icon(
-                            Icons.check_circle_outline_rounded,
-                            size: 30,
-                            color: widget.complete == 1
-                                ? Colors.green
-                                : Colors.white38,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 4.0, sigmaY: 4.0),
+          child: PopUp(
+              bg: Color(0xAAAAAAAA),
+              radius: BorderRadius.circular(10),
+              border: Border(
+                  right: BorderSide(color: Colors.blueAccent, width: 1),
+                  left: BorderSide(color: Colors.blueAccent, width: 1),
+                  top: BorderSide(color: Colors.blueAccent, width: 1),
+                  bottom: BorderSide(color: Colors.blueAccent, width: 1)),
+              actions: [
+                Container(
+                  alignment: Alignment.bottomRight,
+                  child: Button(
+                    click: () {
+                      widget.updateDate(context);
+                    },
+                    contentbtn: Text("Actualizar",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                            fontFamily: 'inter',
+                            fontWeight: FontWeight.bold)),
+                    style: ButtonStyle(
+                        shape: WidgetStatePropertyAll<OutlinedBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16))),
+                        backgroundColor:
+                            WidgetStatePropertyAll<Color>(Colors.blueAccent)),
+                  ),
+                )
+              ],
+              child: Material(
+                color: Colors.transparent,
+                child: Container(
+                    margin: EdgeInsets.only(top: 15),
+                    decoration: BoxDecoration(color: Colors.transparent),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        spacing: 10,
+                        children: [
+                          TextInputKoder(
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                fillColor: Colors.white12,
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: const BorderSide(
+                                      color: Colors.white, width: 1),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: const BorderSide(
+                                      color: Colors.blueAccent, width: 2),
+                                ),
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(1.0),
+                                    borderSide: BorderSide(
+                                        color: Colors.blueAccent, width: 2)),
+                              ),
+                              onChange: widget.changeTitle,
+                              value: TextEditingController(text: widget.title)),
+                          TextBoxs(
+                              height_container: 220,
+                              max_lines: 10,
+                              min_lines: 8,
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                fillColor: Colors.white12,
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: const BorderSide(
+                                      color: Colors.white, width: 1),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: const BorderSide(
+                                      color: Colors.blueAccent, width: 2),
+                                ),
+                                hintStyle: TextStyle(
+                                  color: Colors.grey[400],
+                                ),
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                    borderSide: BorderSide(
+                                        color: Color(0xBCC1CAFF), width: 2)),
+                                contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 15, horizontal: 15),
+                              ),
+                              onChange: widget.changeDescription,
+                              value: TextEditingController(
+                                  text: widget.description)),
+                          Column(
+                            spacing: 8,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                alignment: Alignment.bottomLeft,
+                                child: Select(
+                                    onchange: widget.changePriority,
+                                    decoration: ShadDecoration(
+                                        color: Colors.white,
+                                        border: ShadBorder(
+                                            radius: BorderRadius.circular(8.0),
+                                            right: ShadBorderSide(
+                                                color: Colors.blueAccent,
+                                                width: 2),
+                                            left: ShadBorderSide(
+                                                color: Colors.blueAccent,
+                                                width: 2),
+                                            top: ShadBorderSide(
+                                                color: Colors.blueAccent,
+                                                width: 2),
+                                            bottom: ShadBorderSide(
+                                                color: Colors.blueAccent,
+                                                width: 2))),
+                                    options: {
+                                      "1": 'Alta',
+                                      "2": 'Media',
+                                      "3": 'Baja'
+                                    },
+                                    placeholder: widget.priority == 1
+                                        ? Text("Alta")
+                                        : widget.priority == 2
+                                            ? Text("Media")
+                                            : Text("Baja")),
+                              ),
+                              Button(
+                                click: () {
+                                  changeStateTask();
+                                },
+                                style: ButtonStyle(
+                                  shape: WidgetStatePropertyAll<OutlinedBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(16))),
+                                  backgroundColor:
+                                      WidgetStatePropertyAll<Color>(
+                                        widget.complete == 1
+                                            ? Colors.green
+                                            : Colors.transparent),
+                                  padding: WidgetStatePropertyAll<EdgeInsets>(
+                                      EdgeInsets.symmetric(
+                                          horizontal: 30, vertical: 10)),
+                                  minimumSize:
+                                      WidgetStatePropertyAll<Size>(Size(0, 0)),
+
+                                ),
+                                contentbtn: Text(
+                                  widget.complete == 1
+                                      ? "Completada"
+                                      : "Completadar",
+                                  style: TextStyle(color: widget.complete == 1
+                                      ? Colors.white
+                                      : Colors.blueAccent),
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    )),
               )),
-        ));
+        ),
+      ),
+    );
   }
 }
