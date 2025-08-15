cdf47c2c018bef782b2b35675fff4dbbb3f21cfa
haroldCoder <haroldc2005@gmail.com>
2025-08-15 11:07:10 -0500

    feature: change with languague settings view

---
+++ lib/src/shared/lang/settings/lang.dart
@@ -0,0 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
+
+String get titleSettings => tr("settings.title");
+String get username => tr("settings.username");
+String get email => tr("settings.email");
+String get mode => tr("settings.mode");
+String get changePremium => tr("settings.change_premium");
+String get logOut => tr("settings.log_out");
